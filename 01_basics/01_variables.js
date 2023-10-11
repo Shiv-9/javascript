fd4d0aec9a5e8647da7115a2f18a45f0d94be41b
Shiv-9 <102860408+Shiv-9@users.noreply.github.com>
2023-10-11 06:32:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+ const accountId=345432 // for declaring constants 
+ let accountMail= "shivam@gmail.com"
+ var accountPassword = "123456"
+ accountCity = "Lucknow"
+
+ //accountId = 456 not allowed as it is a constanmt value 
+
+ accountMail = "ST@gmail.com"
+ accountPassword = "23232"
+ accountCity = "Delhi"  // not recommended
+let accountState
+
+
+ console.log(accountId);
+ console.table([accountId,accountMail,accountPassword,accountCity,accountState]);
+ 
+
+
+ /*
+ Prefer using let because of the block scope of let 
+ and functional scope of var. 
+ */
+ 
+
+ 
+ 
+
