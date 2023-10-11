aee57e64daec4c454df05df782420f1784a89bd3
Shiv-9 <102860408+Shiv-9@users.noreply.github.com>
2023-10-11 06:32:59 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+ "use strict"; //treat all js code as newer version of js.
+ 
+
+// alert("shivam") //it's part of browser not node 
+
+
+ let firstVar
+ let secondVar=null
+ let name="shivam"
+ let age= 22
+ let logged = true
+
+/*
+undefined => when a variable is ont assigned any value
+null => it's a standalone value , as well as a object. 
+String => ""
+number => -2^53 + 1 to 2^53 - 1 , for displaying even decimal numbers
+bigint => when value is grater than the range of number
+boolean => true/false
+symbol  => used to identyfy different components as unique .
+*/ 
+
+console.log(typeof firstVar);
+console.log(typeof secondVar);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof logged);
+
+
+
